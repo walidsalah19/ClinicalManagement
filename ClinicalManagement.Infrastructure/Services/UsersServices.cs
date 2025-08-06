bfbb216cd94d55b6be6d0c38c46e90a69334d364
walid salah <walidsalah9585@gmail.com>
2025-08-06 08:17:10 +0300

    transaction

--- ClinicalManagement.Infrastructure/Services/UsersServices.cs
+++ ClinicalManagement.Infrastructure/Services/UsersServices.cs
@@ -1,7 +1,9 @@
 ﻿using ClinicalManagement.Application.Abstractions.Services;
 using ClinicalManagement.Application.Common.Result;
 using ClinicalManagement.Domain.Entities;
+using ClinicalManagement.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,37 +15,46 @@ namespace ClinicalManagement.Infrastructure.Services
     public class UsersServices : IUsersServices
     {
         private readonly UserManager<UsersModel> userManager;
+        private readonly AppDbContext appDbContext;
 
-        public UsersServices(UserManager<UsersModel> userManager)
+        public UsersServices(UserManager<UsersModel> userManager, AppDbContext appDbContext)
         {
             this.userManager = userManager;
+            this.appDbContext = appDbContext;
         }
 
         public async Task<Result<string>> CreateUserAsync(UsersModel user, string role)
         {
-            var createResult = await userManager.CreateAsync(user);
-
-            if (!createResult.Succeeded)
+            using (var transaction = appDbContext.Database.BeginTransaction())
             {
-                var createErrors = createResult.Errors
-                    .Select(e => new Error(e.Description, e.Code))
-                    .ToList();
-                return Result<string>.Failure(createErrors);
+
+                var createResult = await userManager.CreateAsync(user);
+
+                if (!createResult.Succeeded)
+                {
+                   // await transaction.RollbackAsync();
+                    var createErrors = createResult.Errors
+                        .Select(e => new Error(e.Description, e.Code))
+                        .ToList();
+                    return Result<string>.Failure(createErrors);
+                }
+
+                var roleResult = await userManager.AddToRoleAsync(user, role);
+                if (!roleResult.Succeeded)
+                {
+                    await transaction.RollbackAsync();
+
+                    // rollback: delete the user if role assignment failed
+                  //  await userManager.DeleteAsync(user);
+
+                    var roleErrors = roleResult.Errors
+                        .Select(e => new Error(e.Description, e.Code))
+                        .ToList();
+                    return Result<string>.Failure(roleErrors);
+                }
+                await transaction.CommitAsync();
+                return Result<string>.Success("User created successfully.");
             }
-
-            var roleResult = await userManager.AddToRoleAsync(user, role);
-            if (!roleResult.Succeeded)
-            {
-                // rollback: delete the user if role assignment failed
-                await userManager.DeleteAsync(user);
-
-                var roleErrors = roleResult.Errors
-                    .Select(e => new Error(e.Description, e.Code))
-                    .ToList();
-                return Result<string>.Failure(roleErrors);
-            }
-
-            return Result<string>.Success("User created successfully.");
         }
 
 
