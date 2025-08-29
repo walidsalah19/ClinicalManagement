651be2df2677c453595cde85bbb250756524b452
walid salah <walidsalah9585@gmail.com>
2025-08-30 00:17:10 +0300

    appointments update

--- ClinicalManagement.App/Appointments/GetAppointmentsByDate/GetAppointmentsByDateQuery.cs
+++ ClinicalManagement.App/Appointments/GetAppointmentsByDate/GetAppointmentsByDateQuery.cs
@@ -8,12 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClinicalManagement.Application.Appointments.GetAppointments
+namespace ClinicalManagement.Application.Appointments.GetAppointmentsByDate
 {
-     public class GetApplointmentsQuery :IRequest<Result<List<GetAppointmentDto>>>
+    public class GetAppointmentsByDateQuery : IRequest<Result<List<GetAppointmentDto>>>
     {
         [RegularExpression("^(Patient|Doctor)$", ErrorMessage = "type must be Patient or Doctor.")]
         public required string type { get; set; }
         public required string Id { get; set; }
+        public required DateTime From { get; set; }
+        public DateTime To { get; set; } = DateTime.UtcNow;
+
     }
 }
